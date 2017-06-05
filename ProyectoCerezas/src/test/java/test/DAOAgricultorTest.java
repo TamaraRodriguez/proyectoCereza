4c96a37269fe53413d11ad053bc2b82626c369e0
Arti <artemisa.ao@gmail.com>
2017-06-05 14:25:16 +0200

    Añadido el test para agricultor create y read(nSocio)

---
+++ ProyectoCerezas/src/test/java/test/DAOAgricultorTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.util.Properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import dao.DAOAgricultor;
+import junit.framework.TestCase;
+import modelos.Agricultor;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:Spring-Beans.xml"})
+public class DAOAgricultorTest extends TestCase {
+
+	@Autowired
+	DAOAgricultor dao;
+	
+	@Test
+	public void testCreate(){
+		
+		Agricultor a=new Agricultor(-1, "B45263965", "Cerezas S.A.", null, "toledo", "689526341", "cerezas@gmail.com", -1, false);
+		
+		Properties p=System.getProperties();
+		System.out.println(p.getProperty("java.class.path"));
+		dao.create(a);
+		
+		Agricultor u=dao.read(a.getnSocio());
+		
+		assertEquals(a.getCifNif(),u.getCifNif());
+		assertEquals(a.getNombreRazonSocial(),u.getNombreRazonSocial());
+		assertEquals(a.getApellidos(),u.getApellidos());
+		assertEquals(a.getDireccion(),u.getDireccion());
+		assertEquals(a.getTelefono(), u.getTelefono());
+		assertEquals(a.getEmail(), u.getEmail());
+		assertEquals(a.getnSocio(),u.getnSocio());
+		assertEquals(a.isBaja(),u.isBaja());
+		 
+	}
+	
+}
